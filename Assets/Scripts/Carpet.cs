9028afc4c2ccdfb16924e1d1b87618cf303e59db
DanyDiab <danydiab04@gmail.com>
2024-08-02 01:05:31 -0300

    added functionality to the carpet by making a parent class called interactable, for now it just opens a basic menu

--- Assets/Scripts/Carpet.cs
+++ Assets/Scripts/Carpet.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Carpet : MonoBehaviour
+public class Carpet : Interactable
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -6,22 +6,44 @@ public class Interactable : MonoBehaviour
 {
     public bool isBeingInteracted;
     public bool openMenu;
+    public bool playerInRange;
+    public GameObject menuPanel;
+    public Player player;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         isBeingInteracted = false;
         openMenu = false;
+        playerInRange = false;
+        menuPanel.SetActive(openMenu);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(openMenu){
-            
+    void Update(){
+        if(playerInRange){
+            checkInput();
         }
     }
     void interact(){
         isBeingInteracted = !isBeingInteracted;
         openMenu = !openMenu;
+        menuPanel.SetActive(openMenu);
+        player.directionLocked = !player.directionLocked;
+        player.shootLocked = !player.shootLocked;
+    }
+    void checkInput(){
+        if(Input.GetKeyDown(KeyCode.F)){
+            interact();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.CompareTag("Player")){
+            playerInRange = true;
+        }
+    }
+    
+    void OnTriggerExit2D(Collider2D other){
+        if(other.CompareTag("Player")){
+            playerInRange = false;
+        }
     }
 }
